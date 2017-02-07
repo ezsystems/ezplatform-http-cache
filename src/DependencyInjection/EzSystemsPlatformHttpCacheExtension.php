5cc79931498b2e2ca9b4acb43d529d0231e9cd77
André R <ar@ez.no>
2017-02-07 22:25:14 +0100

    Added TagAware Cache Store

--- src/DependencyInjection/EzSystemsPlatformHttpCacheExtension.php
+++ src/DependencyInjection/EzSystemsPlatformHttpCacheExtension.php
@@ -15,6 +15,8 @@ class EzSystemsPlatformHttpCacheExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('default_settings.yml');
+        $loader->load('services.yml');
         $loader->load('view_cache.yml');
     }
 }

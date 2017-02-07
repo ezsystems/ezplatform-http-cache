88751a46998bf71c2804a693b45e2169761d0bf9
Bertrand Dunogier <bertrand.dunogier@ez.no>
2017-02-07 22:19:07 +0100

    Initialized the bundle

---
+++ src/DependencyInjection/EzSystemsPlatformHttpCacheExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace EzSystems\PlatformHttpCacheBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class EzSystemsPlatformHttpCacheExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+    }
+}
---
+++ src/EzSystemsPlatformHttpCacheBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EzSystems\PlatformHttpCacheBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzSystemsPlatformHttpCacheBundle extends Bundle
+{
+}

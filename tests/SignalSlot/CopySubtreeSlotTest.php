77f99f2dd5ddee34d1dea32e4fe79b5c37a41568
Adam Wójs <adam.wojs@ez.no>
2017-10-31 11:41:55 +0100

    Fix EZP-28160: Added HTTP Cache purge after copying subtree

---
+++ tests/SignalSlot/CopySubtreeSlotTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\PlatformHttpCacheBundle\Tests\SignalSlot;
+
+use EzSystems\PlatformHttpCacheBundle\SignalSlot\CopySubtreeSlot;
+use eZ\Publish\Core\SignalSlot\Signal\LocationService\CopySubtreeSignal;
+
+class CopySubtreeSlotTest extends AbstractContentSlotTest
+{
+    private $subtreeId = 67;
+    private $targetParentLocationId = 43;
+    private $targetNewSubtreeId = 45;
+
+    public function createSignal()
+    {
+        return new CopySubtreeSignal([
+            'subtreeId' => $this->subtreeId,
+            'targetParentLocationId' => $this->targetParentLocationId,
+            'targetNewSubtreeId' => $this->targetNewSubtreeId
+        ]);
+    }
+
+    public function generateTags()
+    {
+        return [
+            'location-' . $this->targetParentLocationId,
+            'parent-' . $this->targetParentLocationId
+        ];
+    }
+
+    public function getSlotClass()
+    {
+        return CopySubtreeSlot::class;
+    }
+
+    public function getReceivedSignalClasses()
+    {
+        return [CopySubtreeSignal::class];
+    }
+
+}

54460aa4b84340a3a5398acbffbeb5c3d83bd3ce
André R <ar@ez.no>
2017-11-17 17:07:28 +0100

    EZP-28126: Align AppCache with Varnish behaviour

--- src/AppCache.php
+++ src/AppCache.php
@@ -7,6 +7,9 @@ namespace EzSystems\PlatformHttpCacheBundle;
 
 use eZ\Bundle\EzPublishCoreBundle\HttpCache;
 use EzSystems\PlatformHttpCacheBundle\Proxy\TagAwareStore;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Custom AppCache.
@@ -18,5 +21,51 @@ class AppCache extends HttpCache
     protected function createStore()
     {
         return new TagAwareStore($this->cacheDir ?: $this->kernel->getCacheDir() . '/http_cache');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+    {
+        $response = parent::handle($request, $type, $catch);
+
+        if (!$this->getKernel()->isDebug()) {
+            $this->cleanupHeadersForProd($response);
+        }
+
+        return $response;
+    }
+
+    protected function cleanupHeadersForProd(Response $response)
+    {
+        // remove headers that identify the content or internal digest info
+        $response->headers->remove('xkey');
+        $response->headers->remove('x-content-digest');
+
+        // remove vary by X-User-Hash header
+        $varyValues = [];
+        $variesByUser = false;
+        foreach ($response->getVary() as $value) {
+            if ($value === 'X-User-Hash') {
+                $variesByUser = true;
+            } else {
+                $varyValues[] = $value;
+            }
+        }
+
+        // update resulting vary header in normalized form (comma separated)
+        if (empty($varyValues)) {
+            $response->headers->remove('Vary');
+        } else {
+            $response->headers->set('Vary', implode(', ', $varyValues));
+        }
+
+        // If cache varies by user hash, then make sure other proxies don't cache this
+        if ($variesByUser) {
+            $response->setPrivate();
+            $response->headers->removeCacheControlDirective('s-maxage');
+            $response->headers->addCacheControlDirective('no-cache');
+        }
     }
 }

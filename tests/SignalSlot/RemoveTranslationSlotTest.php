c73eb1a65f5c35c572271276ddd8f2d1ec8d45eb
André R <ar@ez.no>
2018-02-26 12:06:02 +0100

    [Tests] Adapt + minor cleanup

--- tests/SignalSlot/RemoveTranslationSlotTest.php
+++ tests/SignalSlot/RemoveTranslationSlotTest.php
@@ -14,15 +14,10 @@ use eZ\Publish\SPI\Persistence\Content\Location\Handler as LocationHandler;
 /**
  * Test RemoveTranslationSlot for HttpCache.
  */
-class RemoveTranslationSlotTest extends AbstractContentSlotTest
+class RemoveTranslationSlotTest extends AbstractPublishSlotTest
 {
     protected $locationId = 43;
     protected $parentLocationId = 45;
-
-    /**
-     * @var \eZ\Publish\SPI\Persistence\Content\Location\Handler|\PHPUnit_Framework_MockObject_MockObject
-     */
-    private $locationHandlerMock;
 
     /**
      * Check if required signal exists due to BC.
@@ -32,36 +27,6 @@ class RemoveTranslationSlotTest extends AbstractContentSlotTest
         if (!class_exists(RemoveTranslationSignal::class)) {
             self::markTestSkipped('RemoveTranslationSignal does not exist');
         }
-    }
-
-    public function setUp()
-    {
-        $this->locationHandlerMock = $this->createMock(LocationHandler::class);
-        parent::setUp();
-    }
-
-    /**
-     * @dataProvider getReceivedSignals
-     * @param \eZ\Publish\Core\SignalSlot\Signal $signal
-     */
-    public function testReceivePurgesCacheForTags($signal)
-    {
-        $this->locationHandlerMock
-            ->expects($this->once())
-            ->method('loadLocationsByContent')
-            ->with($this->contentId)
-            ->willReturn(
-                [
-                    new Location(
-                        [
-                            'id' => $this->locationId,
-                            'parentId' => $this->parentLocationId,
-                        ]
-                    ),
-                ]
-            );
-
-        parent::testReceivePurgesCacheForTags($signal);
     }
 
     public function createSignal()
@@ -74,18 +39,6 @@ class RemoveTranslationSlotTest extends AbstractContentSlotTest
         );
     }
 
-    public function generateTags()
-    {
-        return [
-            'content-' . $this->contentId,
-            'relation-' . $this->contentId,
-            'location-' . $this->locationId,
-            'parent-' . $this->locationId,
-            'location-' . $this->parentLocationId,
-            'parent-' . $this->parentLocationId,
-        ];
-    }
-
     public function getSlotClass()
     {
         return RemoveTranslationSlot::class;
@@ -94,13 +47,5 @@ class RemoveTranslationSlotTest extends AbstractContentSlotTest
     public function getReceivedSignalClasses()
     {
         return [RemoveTranslationSignal::class];
-    }
-
-    protected function createSlot()
-    {
-        return new RemoveTranslationSlot(
-            $this->purgeClientMock,
-            $this->locationHandlerMock
-        );
     }
 }

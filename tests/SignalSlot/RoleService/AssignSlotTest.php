b19f5e2345ae037bb95636dd3931c2a2fcf2e9fc
André R <ar@ez.no>
2018-04-10 08:39:31 +0200

    [Tests] Add unit tests for new RoleService Slots

---
+++ tests/SignalSlot/RoleService/AssignSlotTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\PlatformHttpCacheBundle\Tests\SignalSlot\RoleService;
+
+use eZ\Publish\Core\SignalSlot\Signal\RoleService\AssignRoleToUserGroupSignal;
+use eZ\Publish\Core\SignalSlot\Signal\RoleService\AssignRoleToUserSignal;
+use EzSystems\PlatformHttpCacheBundle\SignalSlot\RoleService\AssignSlot;
+
+class AssignSlotTest extends AbstractPermissionSlotTest
+{
+    public function createSignal()
+    {
+        return new AssignRoleToUserGroupSignal([
+            'roleId' => 2,
+        ]);
+    }
+
+    public function getReceivedSignalClasses()
+    {
+        return [
+            AssignRoleToUserGroupSignal::class,
+            AssignRoleToUserSignal::class,
+        ];
+    }
+
+    public function getSlotClass()
+    {
+        return AssignSlot::class;
+    }
+}

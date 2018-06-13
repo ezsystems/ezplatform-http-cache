3d22ca62f756a1e8d236721684530005faad08a0
André R <ar@ez.no>
2018-06-13 12:03:36 +0200

    Inital version of Twig ez_http_tag_location() function
    1. Refactors Response taggers to get rid of (by now) unused configurator and reponse, in order to:
    2. Expose Twig extension to have function for tagging locations
    
    Reason for picking location: It has all data needed, we need lcoation in order to be
    able to know which subtree to tag for (to make sure cache is cleared on subtree operations).
    
    If needed there could be a helper for contentInfo as well which loads main for you.

---
+++ src/Twig/ContentTaggingExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * File containing the ContentTaggingExtension class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\PlatformHttpCacheBundle\Twig;
+
+use eZ\Publish\API\Repository\Values\Content\Location;
+use EzSystems\PlatformHttpCacheBundle\ResponseTagger\ResponseTagger;
+use Twig_Extension;
+use Twig_SimpleFunction;
+
+/**
+ * Twig content extension for eZ Publish specific usage.
+ * Exposes helpers to play with public API objects.
+ */
+class ContentTaggingExtension extends Twig_Extension
+{
+    /** @var \EzSystems\PlatformHttpCacheBundle\ResponseTagger\ResponseTagger */
+    protected $responseTagger;
+
+    public function __construct(ResponseTagger $responseTagger)
+    {
+        $this->responseTagger = $responseTagger;
+    }
+
+    /**
+     * @return array|\Twig_Function[]
+     */
+    public function getFunctions()
+    {
+        return [
+            new Twig_SimpleFunction(
+                'ez_http_tag_location',
+                [$this, 'tagHttpCacheForLocation']
+            ),
+        ];
+    }
+
+    /**
+     * Adds tags to current response.
+     *
+     * @internal Function is only for use within this class (and implicit by Twig).
+     *
+     * @param Location $location
+     */
+    public function tagHttpCacheForLocation(Location $location)
+    {
+        $this->responseTagger->tag($location);
+        $this->responseTagger->tag($location->getContentInfo());
+    }
+}

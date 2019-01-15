68535d2c9d8960b181af9f6e1c930b4c75cece63
André R <andre.romcke@gmail.com>
2019-01-15 20:18:58 +0100

    EZP-29194: Improve repo prefix handling for purgeAll() and user context hash tagging (#73)
    Add un prefixed `ez-all` when prefixing is done so purgeAll() can always safely clear ez-all to clear cache across all repos. Also make sure to prefix context hash tagging so clearing it works when having separate repo.

---
+++ tests/PurgeClient/RepositoryPrefixDecoratorTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * File containing the RepositoryPrefixDecoratorTest class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\PlatformHttpCacheBundle\Tests\PurgeClient;
+
+use EzSystems\PlatformHttpCacheBundle\PurgeClient\PurgeClientInterface;
+use EzSystems\PlatformHttpCacheBundle\PurgeClient\RepositoryPrefixDecorator;
+use PHPUnit\Framework\TestCase;
+
+class RepositoryPrefixDecoratorTest extends TestCase
+{
+    public function testPurge()
+    {
+        $purgeClient = $this->createMock(PurgeClientInterface::class);
+        $purgeClient
+            ->expects($this->once())
+            ->method('purge')
+            ->with($this->equalTo(['location-123', 'content-44', 'ez-all']));
+
+        $prefixDecorator = new RepositoryPrefixDecorator($purgeClient);
+        $prefixDecorator->purge([123, 'content-44', 'ez-all']);
+    }
+
+    public function testPurgeWithPrefix()
+    {
+        $purgeClient = $this->createMock(PurgeClientInterface::class);
+        $purgeClient
+            ->expects($this->once())
+            ->method('purge')
+            ->with($this->equalTo(['intranet_location-123', 'intranet_content-44', 'intranet_ez-all']));
+
+        $prefixDecorator = new RepositoryPrefixDecorator($purgeClient);
+        $prefixDecorator->setRepositoryId('intranet');
+        $prefixDecorator->purge([123, 'content-44', 'ez-all']);
+    }
+
+    public function testPurgeAll()
+    {
+        $purgeClient = $this->createMock(PurgeClientInterface::class);
+        $purgeClient
+            ->expects($this->once())
+            ->method('purgeAll');
+
+        $prefixDecorator = new RepositoryPrefixDecorator($purgeClient);
+        $prefixDecorator->purgeAll();
+    }
+
+    public function testPurgeAllWithPrefix()
+    {
+        $purgeClient = $this->createMock(PurgeClientInterface::class);
+        $purgeClient
+            ->expects($this->once())
+            ->method('purgeAll');
+
+        $prefixDecorator = new RepositoryPrefixDecorator($purgeClient);
+        $prefixDecorator->setRepositoryId('intranet');
+        $prefixDecorator->purgeAll();
+    }
+}

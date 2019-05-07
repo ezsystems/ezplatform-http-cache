0466c9fa0b93b398286276c8b4c6d4ba67bf7817
Dawid Parafiński <dawid.parafinski@ez.no>
2019-05-07 15:35:43 +0200

    EZP-28034: Create ezsystems/ezplatform-rest-common package

--- src/EventSubscriber/RestKernelViewSubscriber.php
+++ src/EventSubscriber/RestKernelViewSubscriber.php
@@ -7,7 +7,7 @@ namespace EzSystems\PlatformHttpCacheBundle\EventSubscriber;
 
 use eZ\Publish\API\Repository\Values\Content\Section;
 use eZ\Publish\API\Repository\Values\ContentType\ContentType;
-use eZ\Publish\Core\REST\Common\Values\Root;
+use EzSystems\EzPlatformRestCommon\Values\Root;
 use eZ\Publish\Core\REST\Server\Values\CachedValue;
 use eZ\Publish\Core\REST\Server\Values\ContentTypeGroupList;
 use eZ\Publish\Core\REST\Server\Values\ContentTypeGroupRefList;

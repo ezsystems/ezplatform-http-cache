9ad835bfeae010b072e06bc0127ec0d6c0098594
Marek Nocoń <mnocon@users.noreply.github.com>
2020-09-10 11:19:36 +0200

    [Tests] Added ClockMock to make time-sensitive tests more robust

--- tests/PurgeClient/LocalPurgeClientTest.php
+++ tests/PurgeClient/LocalPurgeClientTest.php
@@ -4,29 +4,23 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
-namespace EzSystems\PlatformHttpCacheBundle\Tests\PurgeClient;
-
-/**
- * Avoid test failure caused by time passing between generating expected & actual object.
- *
- * @return int
- */
-function time()
-{
-    return 1417624982;
-}
-
 namespace eZ\Publish\Core\MVC\Symfony\Cache\Tests;
 
 use EzSystems\PlatformHttpCacheBundle\RequestAwarePurger;
 use EzSystems\PlatformHttpCacheBundle\PurgeClient\LocalPurgeClient;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bridge\PhpUnit\ClockMock;
 
 class LocalPurgeClientTest extends TestCase
 {
+    /**
+     * @group time-sensitive
+     */
     public function testPurge()
     {
+        ClockMock::register(Request::class);
+
         $locationIds = [123, 456, 789];
         $expectedBanRequest = Request::create('http://localhost', 'PURGE');
         $expectedBanRequest->headers->set('key', 'l123 l456 l789');

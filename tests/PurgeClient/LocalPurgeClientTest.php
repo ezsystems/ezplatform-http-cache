1e2b8403878e66992e34679fad7637be0bf88a84
Dawid Parafiński <dawid.parafinski@ez.no>
2021-10-18 15:09:32 +0200

    IBX-913: Fixed test namespace

--- tests/PurgeClient/LocalPurgeClientTest.php
+++ tests/PurgeClient/LocalPurgeClientTest.php
@@ -4,7 +4,7 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
-namespace eZ\Publish\Core\MVC\Symfony\Cache\Tests;
+namespace EzSystems\PlatformHttpCacheBundle\Tests\PurgeClient;
 
 use EzSystems\PlatformHttpCacheBundle\RequestAwarePurger;
 use EzSystems\PlatformHttpCacheBundle\PurgeClient\LocalPurgeClient;
